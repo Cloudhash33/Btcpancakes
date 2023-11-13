d802231ac1f40eb974c20c76485d3ab12e37233d
Cloudhash33 <149048907+Cloudhash33@users.noreply.github.com>
2023-11-13 13:35:32 -1000

    Update App.js

--- Public/App.js
+++ Public/App.js
@@ -1,19 +1,43 @@
-async function connectWallet() {
-    if (window.ethereum) { // For MetaMask
+async function connectWallet(swapDirection, amount) {
+    // Check for MetaMask
+    if (window.ethereum) {
         try {
             await window.ethereum.request({ method: 'eth_requestAccounts' });
             console.log('Connected to MetaMask.');
         } catch (error) {
             console.error('User denied account access to MetaMask:', error);
+            return; // Exit the function if user denies access
         }
-    } else if (window.walletconnect) { // For WalletConnect
+    } 
+    // Check for WalletConnect
+    else if (window.walletconnect) {
         // Implement WalletConnect logic
-    } else {
+        // This typically involves creating a new WalletConnect instance
+        // and triggering the QR code modal for connection
+        // Example:
+        // const walletConnect = new WalletConnect({ bridge: "https://bridge.walletconnect.org" });
+        // if (!walletConnect.connected) {
+        //     await walletConnect.createSession();
+        // }
+    } 
+    else {
         console.log('No Ethereum wallet found. Install MetaMask or use WalletConnect.');
+        return; // Exit the function if no wallet is detected
     }
 
-    // Example: POST request to your backend
-    fetch('/thorswap/swapBTCtoBSC', {
+    // Determine the endpoint based on the swap direction
+    let endpoint = '';
+    if (swapDirection === 'btcToBsc') {
+        endpoint = '/thorswap/swapBTCtoBSC';
+    } else if (swapDirection === 'bscToBtc') {
+        endpoint = '/thorswap/swapBSCtoBTC';
+    } else {
+        console.error('Invalid swap direction');
+        return; // Exit the function for invalid swap direction
+    }
+
+    // POST request to your backend
+    fetch(endpoint, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -22,7 +46,7 @@ async function connectWallet() {
     })
     .then(response => response.json())
     .then(data => {
-        document.getElementById('status').innerText = 'Swap successful: ' + JSON.stringify(data);
+        document.getElementById('status').innerText = `Swap successful: ${JSON.stringify(data)}`;
     })
     .catch(error => {
         console.error('Error:', error);
