5dde9c39f3cac79ae084e345181c8be318da688a
Cloudhash33 <149048907+Cloudhash33@users.noreply.github.com>
2023-11-12 21:23:36 -1000

    Update Thorswap.js

--- Thorswap.js
+++ Thorswap.js
@@ -1,31 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const { SwapKit } = require('@swapkit/sdk');
+const { createSwapKit, Chain } = require('@swapkit/sdk');
 const Web3 = require('web3');
 const web3 = new Web3(process.env.INFURA_URL);
 const contractABI = require('../path/to/your/contractABI.json');
 const contractAddress = 'YOUR_CONTRACT_ADDRESS';
 
-const swapKit = new SwapKit(/* configuration parameters */);
+// Configure the SwapKit client
+const config = { /* ... configuration details ... */ };
+const skClient = createSwapKit(config);
 
 router.post('/swapBTCtoBNB', async (req, res) => {
     const { amount } = req.body;
-    const feePercentage = 0.001; // 0.1%
-    const feeAmount = amount * feePercentage;
+    const feeAmount = calculateFee(amount);
     const amountAfterFee = amount - feeAmount;
 
     try {
-        // Use SwapKit SDK to perform the swap
-        const swapResult = await swapKit.executeSwap({
-            // Swap details
-        });
-        // Handle swap result
+        await skClient.connectLedger(Chain.BTC);
+
+        // Perform the swap using the SwapKit SDK
+        const swapDetails = {
+            // Details for the swap
+        };
+        const txHash = await skClient.swap(swapDetails);
 
         // Send fee to FeeCollector contract
         const feeCollectorContract = new web3.eth.Contract(contractABI, contractAddress);
         // Logic to send the fee to the contract
 
-        res.json({ message: 'Swap initiated', swapResult });
+        res.json({ message: 'Swap initiated', txHash });
     } catch (error) {
         console.error('Error in swap:', error);
         res.status(500).send('Error occurred during the swap');
