8518457bd99269ba3b55a21272f71f66b04bade4
Cloudhash33 <149048907+Cloudhash33@users.noreply.github.com>
2023-11-12 20:20:37 -1000

    Update Feecollector.sol

--- Contracts/Feecollector.sol
+++ Contracts/Feecollector.sol
@@ -1,1 +1,26 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract FeeCollector {
+    address public owner;
+    uint256 public feeBalance;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {
+        feeBalance += msg.value;
+    }
+
+    // Function to withdraw the fees
+    function withdrawFees(address payable recipient, uint256 amount) external {
+        require(msg.sender == owner, "Only owner can withdraw fees");
+        require(amount <= feeBalance, "Insufficient balance");
+
+        feeBalance -= amount;
+        recipient.transfer(amount);
+    }
+}
