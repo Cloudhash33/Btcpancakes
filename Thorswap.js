bf498bab27290444667f75cd17658c124da7eafb
Cloudhash33 <149048907+Cloudhash33@users.noreply.github.com>
2023-11-12 21:02:48 -1000

    Update Thorswap.js

--- Thorswap.js
+++ Thorswap.js
@@ -1,11 +1,12 @@
-npm install thorswap-sdk
-
 const express = require('express');
 const router = express.Router();
+const { SwapKit } = require('@swapkit/sdk');
 const Web3 = require('web3');
 const web3 = new Web3(process.env.INFURA_URL);
 const contractABI = require('../path/to/your/contractABI.json');
 const contractAddress = 'YOUR_CONTRACT_ADDRESS';
+
+const swapKit = new SwapKit(/* configuration parameters */);
 
 router.post('/swapBTCtoBNB', async (req, res) => {
     const { amount } = req.body;
@@ -13,18 +14,22 @@ router.post('/swapBTCtoBNB', async (req, res) => {
     const feeAmount = amount * feePercentage;
     const amountAfterFee = amount - feeAmount;
 
-    // Assuming ThorSwap SDK or API interaction
-    // Perform swap logic here
+    try {
+        // Use SwapKit SDK to perform the swap
+        const swapResult = await swapKit.executeSwap({
+            // Swap details
+        });
+        // Handle swap result
 
-    // Send fee to FeeCollector contract
-    const feeCollectorContract = new web3.eth.Contract(contractABI, contractAddress);
-    feeCollectorContract.methods.receive().send({
-        from: 'YOUR_ACCOUNT_ADDRESS',
-        value: web3.utils.toWei(feeAmount.toString(), 'ether'),
-        // Additional transaction parameters
-    });
+        // Send fee to FeeCollector contract
+        const feeCollectorContract = new web3.eth.Contract(contractABI, contractAddress);
+        // Logic to send the fee to the contract
 
-    res.json({ message: 'Swap initiated', amountAfterFee });
+        res.json({ message: 'Swap initiated', swapResult });
+    } catch (error) {
+        console.error('Error in swap:', error);
+        res.status(500).send('Error occurred during the swap');
+    }
 });
 
 module.exports = router;
