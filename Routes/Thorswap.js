c0047fe9c1ae4253b485840a49adaa1d2f2ac204
Cloudhash33 <149048907+Cloudhash33@users.noreply.github.com>
2023-11-13 14:17:36 -1000

    Update Thorswap.js

--- Routes/Thorswap.js
+++ Routes/Thorswap.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const router = express.Router();
+
 // Include ThorSwap SDK or API Client
+// const thorSwapClient = require('thorswap-sdk-or-api-client');
 
-router.post('/swapBTCtoBNB', async (req, res) => {
-    const { amount } = req.body; // Example: Extract amount from request body
+router.post('/swapBTCtoBSC', async (req, res) => {
+    try {
+        const { amount } = req.body;
 
-    // Logic to perform swap using ThorSwap SDK or API
-    // ...
+        // Logic to perform swap from BTC to BSC
+        // const swapResult = await thorSwapClient.swapBTCtoBSC(amount);
 
-    res.json({ message: 'Swap successful', swappedAmount: /* result from swap */ });
+        res.json({ message: 'Swap BTC to BSC successful', swappedAmount: swapResult });
+    } catch (error) {
+        res.status(500).json({ message: 'Error in swapping BTC to BNB', error: error.message });
+    }
+});
+
+router.post('/swapBSCtoBTC', async (req, res) => {
+    try {
+        const { amount } = req.body;
+
+        // Logic to perform swap from BSC to BTC
+        // const swapResult = await thorSwapClient.swapBNBtoBTC(amount);
+
+        res.json({ message: 'Swap BSC to BTC successful', swappedAmount: swapResult });
+    } catch (error) {
+        res.status(500).json({ message: 'Error in swapping BSC to BTC', error: error.message });
+    }
 });
 
 module.exports = router;
