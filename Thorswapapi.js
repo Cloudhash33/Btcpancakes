7173c3562235ed5ebe5947e3581378fb2ddff912
Cloudhash33 <149048907+Cloudhash33@users.noreply.github.com>
2023-11-16 14:58:24 -1000

    Update Thorswapapi.js

--- Thorswapapi.js
+++ Thorswapapi.js
@@ -4,18 +4,19 @@ const { ethers } = require('ethers');
 const app = express();
 app.use(express.json());
 
-const THORSWAP_API_URL = 'https://api.thorswap.com'; // ThorSwap API URL
-const BSC_NODE_URL = 'https://bsc-dataseed.binance.org/'; // BSC Node URL
+const THORSWAP_API_URL = 'https://api.thorswap.com';
+const BSC_NODE_URL = 'https://bsc-dataseed.binance.org/';
 const PANCAKESWAP_ROUTER_ADDRESS = '...'; // PancakeSwap Router address
 const routerABI = require('./routerABI.json'); // PancakeSwap Router ABI
 
 // Setup provider and wallet
 const provider = new ethers.providers.JsonRpcProvider(BSC_NODE_URL);
-const wallet = new ethers.Wallet('YOUR_PRIVATE_KEY', provider); // Be cautious with private key management
+const wallet = new ethers.Wallet('YOUR_PRIVATE_KEY', provider);
 
 // Create PancakeSwap Router contract instance
 const routerContract = new ethers.Contract(PANCAKESWAP_ROUTER_ADDRESS, routerABI, wallet);
 
+// Endpoint for swapping BTC to a specific token
 app.post('/swapBTCtoToken', async (req, res) => {
     const { btcAmount, tokenAddress } = req.body;
 
@@ -28,18 +29,13 @@ app.post('/swapBTCtoToken', async (req, res) => {
             // Other necessary details
         });
 
-        // Wait for the transaction to complete and get BSC tokens (e.g., BNB)
-        // This may involve waiting for confirmations or using an event listener
-
         // Step 2: Swap BSC token to desired token on PancakeSwap
-        // Setup swap parameters (example with BNB to another token)
         const amountIn = ethers.utils.parseUnits('1', 'ether'); // Example amount
         const amountOutMin = 0; // Set minimum amount of tokens to receive
         const path = ['BNB_ADDRESS', tokenAddress]; // Swap path (BNB to desired token)
         const to = 'RECIPIENT_ADDRESS'; // Address receiving the output tokens
         const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20-minute deadline
 
-        // Perform the swap on PancakeSwap
         const tx = await routerContract.swapExactTokensForTokens(
             amountIn,
             amountOutMin,
@@ -48,8 +44,46 @@ app.post('/swapBTCtoToken', async (req, res) => {
             deadline
         );
 
-        const receipt = await tx.wait(); // Wait for the transaction to be mined
+        const receipt = await tx.wait();
         res.json({ success: true, txHash: receipt.transactionHash });
+    } catch (error) {
+        console.error('Error in swap:', error);
+        res.status(500).send('Error occurred during the swap');
+    }
+});
+
+// New endpoint to swap a Token to BTC
+app.post('/swapTokentoBTC', async (req, res) => {
+    const { tokenAmount, tokenAddress } = req.body;
+
+    try {
+        // Step 1: Swap Token to BSC (BNB) on PancakeSwap
+        const amountIn = ethers.utils.parseUnits(tokenAmount.toString(), 'ether');
+        const amountOutMin = 0; // Set minimum amount of BNB to receive
+        const path = [tokenAddress, 'BNB_ADDRESS']; // Swap path (token to BNB)
+        const to = 'YOUR_WALLET_ADDRESS'; // Address receiving BNB
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
+
+        const swapTx = await routerContract.swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            path,
+            to,
+            deadline
+        );
+
+        await swapTx.wait(); // Wait for the swap to be mined
+
+        // Step 2: Swap BSC (BNB) to BTC using ThorSwap
+        const bnbAmount = ethers.utils.formatUnits(swapTx.value, 'ether');
+        const thorSwapResponse = await axios.post(`${THORSWAP_API_URL}/swap`, {
+            fromChain: 'BSC',
+            toChain: 'BTC',
+            amount: bnbAmount,
+            // Other necessary details
+        });
+
+        res.json({ success: true, txHash: thorSwapResponse.data.txHash });
     } catch (error) {
         console.error('Error in swap:', error);
         res.status(500).send('Error occurred during the swap');
